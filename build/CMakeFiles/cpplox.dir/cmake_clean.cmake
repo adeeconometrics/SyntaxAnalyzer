fe5771efac085260f8e338d26826ade6058665aa
Dave Amiana <amianism25@gmail.com>
2022-05-08 19:29:44 +0800

    CMake configured

---
+++ build/CMakeFiles/cpplox.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpplox.dir/src/ErrorHandler.cpp.o"
+  "CMakeFiles/cpplox.dir/src/FileReader.cpp.o"
+  "CMakeFiles/cpplox.dir/src/Main.cpp.o"
+  "CMakeFiles/cpplox.dir/src/Scanner.cpp.o"
+  "CMakeFiles/cpplox.dir/src/Token.cpp.o"
+  "cpplox"
+  "cpplox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpplox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
